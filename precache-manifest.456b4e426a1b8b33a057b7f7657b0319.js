308c5357c13981bdd4f705ee9c30b79e0c934b0b
aeramu <sulam3010@gmail.com>
2020-09-21 22:08:35 +0700

    Updates

---
+++ precache-manifest.456b4e426a1b8b33a057b7f7657b0319.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6008b6d1f8f1bc088cd",
+    "url": "/spektrum/static/js/app.28137fb0.chunk.js"
+  },
+  {
+    "revision": "f88f98fc804a31449320",
+    "url": "/spektrum/static/js/runtime~app.294e9019.js"
+  },
+  {
+    "revision": "791b0f8bf67395b34cc6",
+    "url": "/spektrum/static/js/2.c7256470.chunk.js"
+  },
+  {
+    "revision": "72fbe78779f887ac089b071968ff60f2",
+    "url": "/spektrum/static/media/logo.4b578565.png"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/spektrum/expo-service-worker.js"
+  },
+  {
+    "revision": "fc61f6781b116e660164130598fb568b",
+    "url": "/spektrum/index.html"
+  },
+  {
+    "revision": "f0331e5138056b6049a6857ff4af4990",
+    "url": "/spektrum/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/spektrum/serve.json"
+  },
+  {
+    "revision": "4f365ab8a87798c639009f0dd1d5aad2",
+    "url": "/spektrum/static/js/2.c7256470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/spektrum/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/spektrum/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/spektrum/favicon.ico"
+  },
+  {
+    "revision": "22ab43e6692e6d8cb3f3145fd54164a9",
+    "url": "/spektrum/manifest.json"
+  }
+];
